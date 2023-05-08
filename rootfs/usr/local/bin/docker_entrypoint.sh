7c19bbb51b59c828e9335b6622a344001d7db6f5
stlouisn <stlouisn@hotmail.com>
2023-05-07 22:09:28 -0300

    Update docker_entrypoint.sh

--- rootfs/usr/local/bin/docker_entrypoint.sh
+++ rootfs/usr/local/bin/docker_entrypoint.sh
@@ -8,3 +8,9 @@ fail2ban-client status
 
 # Start fail2ban in console mode
 exec fail2ban-client -x -vv -f start 
+
+#permissions
+#setfacl -R -m u:10000:r /var/logs
+
+#app
+#exec gosu fail2ban-client -x -vv -f start
