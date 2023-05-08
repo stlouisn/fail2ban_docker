26bf53a4056a87404d6c0b1f70c9ebf73708d9f7
stlouisn <stlouisn@hotmail.com>
2023-05-07 21:57:50 -0300

    Update docker_entrypoint.sh

--- rootfs/usr/local/bin/docker_entrypoint.sh
+++ rootfs/usr/local/bin/docker_entrypoint.sh
@@ -1,40 +1,10 @@
-#!/bin/b a s h
+#!/bin/ba sh
 
-#=========================================================================================
+echo 'Starting Fail2Ban...'
+echo
 
-# Fix user and group ownerships for '/config'
-chown -R transmission:transmission /config
+fail2ban-client set logtarget STDOUT
+fail2ban-client status
 
-# First-run setup
-if
-    [ ! -e "/config/settings.json" ]
-then
-    echo "{" >> /config/settings.json
-    echo "  \"bind-address-ipv4\": \"0.0.0.0\"," >> /config/settings.json
-    echo "  \"bind-address-ipv6\": \"::\"," >> /config/settings.json
-    echo "  \"blocklist-enabled\": false," >> /config/settings.json
-    echo "  \"message-level\": 2," >> /config/settings.json
-    echo "  \"rpc-authentication-required\": false," >> /config/settings.json
-    echo "  \"rpc-bind-address\": \"0.0.0.0\"," >> /config/settings.json
-    echo "  \"rpc-enabled\": true" >> /config/settings.json
-    echo "}" >> /config/settings.json
-fi
-
-# Delete PID if it exists
-if
-    [ -e "/config/transmission.pid" ]
-then
-    rm -f /config/transmission.pid
-fi
-
-#=========================================================================================
-
-# Start transmission in console mode
-exec gosu transmission \
-    /usr/bin/transmission-daemon \
-    --config-dir /config \
-    --pid-file /config/transmission.pid \
-    --no-watch-dir \
-    --incomplete-dir /downloads/incomplete \
-    --download-dir /downloads/complete \
-    --foreground
+# Start fail2ban in console mode
+exec fail2ban-client -x -vv -f start 
